c9431549f469c8ac1c142244871b86ab51cc7b39
mousabmt <mousabtamari0799@gmail.com>
2025-08-12 13:04:57 +0300

    cloudinary

--- server/routes/routers/organizationMembers.js
+++ server/routes/routers/organizationMembers.js
@@ -3,6 +3,8 @@ const inOrg = require('../../middleware/inOrg')
 
 const router=require('express').Router()
 router.get('/:organization_ID',auth,inOrg,async (req,res)=>{
-
+    res.json({
+        message:"hi"
+    })
 })
 module.exports=router
